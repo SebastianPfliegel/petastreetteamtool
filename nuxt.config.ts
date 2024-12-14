f7f1816ba96d8cdbfe508352a75137825cb9fd45
Sebastian Pfliegel <pfliegel.sebastian@gmail.com>
2024-12-15 00:02:40 +0100

    Appwrite module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  appwrite: {
+    project: "675dd9df0020d7f5775a",
+  },
   googleFonts: {
     families: {
       Poppins: true,
     },
   },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "@nuxt/eslint"],
-  runtimeConfig: {
-    public: {
-      endpointUrl: "https://cloud.appwrite.io/v1",
-      projectId: "",
-    },
-  },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@nuxt/eslint",
+    "nuxt-appwrite",
+  ],
 });
