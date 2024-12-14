dcdd498b82b1b0bd8186f66ed32710c7d780f054
Sebastian Pfliegel <pfliegel.sebastian@gmail.com>
2024-12-14 22:03:07 +0100

    Configuration

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-4xl">Hello</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/google-fonts',
-    '@nuxt/eslint'
-  ]
-})
+  googleFonts: {
+    families: {
+      Poppins: true,
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "@nuxt/eslint"],
+  runtimeConfig: {
+    public: {
+      endpointUrl: "",
+      projectId: "",
+    },
+  },
+});
