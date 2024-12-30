0f5f8a7aa2d1ff67041bc7c909386195ee972004
Sebastian Pfliegel <pfliegel.sebastian@gmail.com>
2024-12-30 19:38:43 +0100

    Add appwrite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "nuxt-appwrite"],
+  appwrite: {
+    project: "675dd9df0020d7f5775a",
+  },
 });
